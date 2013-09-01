1461aada65b638482a52d82f002c3463a912f314
Baron Schwartz <baron@xaprb.com>
2013-08-31 21:32:21 -0400

    Create later version of packver to make sure tests work

--- test/packver/packver.go
+++ test/packver/packver.go
@@ -1,5 +1,8 @@
 package packver
 
 func Version() string {
-	return "v0.2.3"
+	// This file's purpose is to be pinned to a specific tag (by one of the
+	// tests) and if that happens correctly, the below line will be instead
+	// "v0.2.3", which it's obviously not if you're reading this comment.
+	return "this version should not be returned"
 }
